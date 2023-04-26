573903778aef239007a37487d99302425ab58153
vinodmalali <70831777+vinodmalali@users.noreply.github.com>
2023-04-26 17:31:55 +0530

    Add files via upload

---
+++ verifier-integration/go/index.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/iden3/go-circuits"
+	auth "github.com/iden3/go-iden3-auth"
+	"github.com/iden3/go-iden3-auth/loaders"
+	"github.com/iden3/go-iden3-auth/pubsignals"
+	"github.com/iden3/go-iden3-auth/state"
+	"github.com/iden3/iden3comm/protocol"
+)
+
+func main() {
+	http.HandleFunc("/api/sign-in", GetAuthRequest)
+	http.HandleFunc("/api/callback", Callback)
+	http.ListenAndServe(":8080", nil)
+}
+
+// Create a map to store the auth requests and their session IDs
+var requestMap = make(map[string]interface{})
+
+func GetAuthRequest(w http.ResponseWriter, r *http.Request) {
+
+	// Audience is verifier id
+	rURL := "<NGROK_URL>"
+	sessionID := 1
+	CallbackURL := "/api/callback"
+	Audience := "did:polygonid:polygon:mumbai:2qDyy1kEo2AYcP3RT4XGea7BtxsY285szg6yP9SPrs"
+
+	uri := fmt.Sprintf("%s%s?sessionId=%s", rURL, CallbackURL, strconv.Itoa(sessionID))
+
+	// Generate request for basic authentication
+	var request protocol.AuthorizationRequestMessage = auth.CreateAuthorizationRequest("test flow", Audience, uri)
+
+	request.ID = "7f38a193-0918-4a48-9fac-36adfdb8b542"
+	request.ThreadID = "7f38a193-0918-4a48-9fac-36adfdb8b542"
+
+	// Add request for a specific proof
+	var mtpProofRequest protocol.ZeroKnowledgeProofRequest
+	mtpProofRequest.ID = 1
+	mtpProofRequest.CircuitID = string(circuits.AtomicQuerySigV2CircuitID)
+	mtpProofRequest.Query = map[string]interface{}{
+		"allowedIssuers": []string{"*"},
+		"credentialSubject": map[string]interface{}{
+			"birthday": map[string]interface{}{
+				"$lt": 20000101,
+			},
+		},
+		"context": "https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld",
+		"type":    "KYCAgeCredential",
+	}
+	request.Body.Scope = append(request.Body.Scope, mtpProofRequest)
+
+	// Store auth request in map associated with session ID
+	requestMap[strconv.Itoa(sessionID)] = request
+
+	// print request
+	fmt.Println(request)
+
+	msgBytes, _ := json.Marshal(request)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(msgBytes)
+	return
+}
+
+// Callback works with sign-in callbacks
+func Callback(w http.ResponseWriter, r *http.Request) {
+
+	// Get session ID from request
+	sessionID := r.URL.Query().Get("sessionId")
+
+	// get JWZ token params from the post request
+	tokenBytes, _ := io.ReadAll(r.Body)
+
+	// Add Polygon Mumbai RPC node endpoint - needed to read on-chain state
+	ethURL := "MUMBAI_RPC_URL"
+
+	// Add identity state contract address
+	contractAddress := "0x134B1BE34911E39A8397ec6289782989729807a4"
+
+	resolverPrefix := "polygon:mumbai"
+
+	// Locate the directory that contains circuit's verification keys
+	keyDIR := "../keys"
+
+	// fetch authRequest from sessionID
+	authRequest := requestMap[sessionID]
+
+	// print authRequest
+	fmt.Println(authRequest)
+
+	// load the verifcation key
+	var verificationKeyloader = &loaders.FSKeyLoader{Dir: keyDIR}
+	resolver := state.ETHResolver{
+		RPCUrl:          ethURL,
+		ContractAddress: common.HexToAddress(contractAddress),
+	}
+
+	resolvers := map[string]pubsignals.StateResolver{
+		resolverPrefix: resolver,
+	}
+
+	// EXECUTE VERIFICATION
+	verifier := auth.NewVerifier(verificationKeyloader, loaders.DefaultSchemaLoader{IpfsURL: "ipfs.io"}, resolvers)
+	authResponse, err := verifier.FullVerify(
+		r.Context(),
+		string(tokenBytes),
+		authRequest.(protocol.AuthorizationRequestMessage),
+		pubsignals.WithAcceptedStateTransitionDelay(time.Minute*5))
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	userID := authResponse.From
+
+	messageBytes := []byte("User with ID " + userID + " Successfully authenticated")
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(messageBytes)
+
+	return
+}
